efa785ee5ac805a75d12c36b1458ac5e88add6c3
JoeArrowood-E99965 <jarrowood@aflac.com>
2021-09-20 05:15:28 -0400

    Work version

--- RegExClient/Properties/AssemblyInfo.cs
+++ RegExClient/Properties/AssemblyInfo.cs
@@ -1,25 +1,28 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RegExClient")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RegExClient")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JoeWare")]
+[assembly: AssemblyTrademark("JoeWare")]
+[assembly: AssemblyTitle("RegExClient")]
+[assembly: AssemblyProduct("RegExClient")]
+[assembly: AssemblyCopyright("Copyright © JoeWare 2021")]
+[assembly: AssemblyDescription("RegEx Client is designed to allow the user to test Regular Expressions.\r\n\r\nNot quite ready yet...\r\n\r\n" +
+                               "This is a work in progress.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("2031e8e7-6b41-4d5a-a774-447da436f9d2")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +35,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- RegExClient/RegExClientForm.cs
+++ RegExClient/RegExClientForm.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using AboutJoeWare_Lib;
 
 namespace RegExClient
 {
@@ -14,44 +15,9 @@ namespace RegExClient
 
         // ------------------------------------------------
 
-        private void ShowMatches()
+        private void OnKeyUp(object sender, KeyEventArgs e)
         {
-            var toggleColor = true;
-
-            try
-            {
-                // -------------------
-                // Look for groupings.
-
-                var regex = GetRegex();
-
-                // ----------------------------
-                // Define the string to search.
-
-                var matchCollection = regex.Matches(tbInput.Text);
-
-                foreach(Match group in matchCollection)
-                {
-                    tbInput.SelectionStart = group.Index;
-                    tbInput.SelectionLength = group.Length;
-                    tbInput.SelectionBackColor = toggleColor ? Color.Cyan : Color.Yellow;
-                    tbInput.SelectionProtected = true;
-
-                    toggleColor = !toggleColor;
-
-                    tbInput.SelectionStart = tbInput.Text.Length;
-                    tbInput.SelectionLength = 0;
-                    tbInput.SelectionBackColor = Color.White;
-                }
-            }
-            catch(Exception /*ex*/)
-            {
-                // Throw away any exception;
-            }
-            finally
-            {
-                toggleColor = true;
-            }
+            ShowMatches();
         }
 
         // ------------------------------------------------
@@ -81,7 +47,7 @@ namespace RegExClient
             tbInput.Text = "01/01/2021 until 12/31/2021";
             //tbInput.Text = @"\\dcaages01\AGES\DEV\AGTracGARMImageOutput\";
 
-            tbRegEx.Text = @"[0-2]?\d[/\-][0-3]?\d[/\-][12]\d{3}";
+            tbRegEx.Text = @"\b(0?[1-9]|1[0-2])[/\-](0?[1-3]|3[0-1])[/\-][12]\d{3}\b";
             //tbRegEx.Text = "(?=^.{8,30}$)(?=.*[A-Z])(?=.*[a-z].*[a-z].*[a-z]).*$";
             //tbRegEx.Text = "(\\\\\\\\([a-z|A-Z|0-9|-|_|\\s]{2,15}){1}(\\.[a-z|A-Z|0-9|-|_|\\s]{1,64}){0,3}){1}(\\\\[^\\\\|\\/|\\*|\\:|\\?|\"|\\<|\\>|\\|]{1,64}){1,}(\\\\){0,}";
 
@@ -90,27 +56,97 @@ namespace RegExClient
 
         // ------------------------------------------------
 
-        private void OnKeyUp(object sender, KeyEventArgs e)
+        private void ShowMatches()
         {
-            ShowMatches();
+            var toggleColor = true;
+            ClearMatches();
+
+            try
+            {
+                // -------------------
+                // Look for groupings.
+
+                var regex = GetRegex();
+
+                // ----------------------------
+                // Define the string to search.
+
+                var matchCollection = regex.Matches(tbInput.Text);
+
+                foreach(Match group in matchCollection)
+                {
+                    tbInput.SelectionStart = group.Index;
+                    tbInput.SelectionLength = group.Length;
+                    tbInput.SelectionBackColor = toggleColor ? Color.Cyan : Color.Aquamarine;
+                    tbInput.SelectionProtected = true;
+
+                    toggleColor = !toggleColor;
+
+                    tbInput.SelectionStart = tbInput.Text.Length;
+                    tbInput.SelectionLength = 0;
+                    tbInput.SelectionBackColor = Color.White;
+                }
+            }
+            catch(Exception /*ex*/)
+            {
+                // Throw away any exception;
+            }
+            finally
+            {
+                toggleColor = true;
+            }
         }
 
         // ------------------------------------------------
 
         private Regex GetRegex()
         {
-            var retVal = null as Regex;
+            var retVal = new Regex(".*");
+            var options = cbMultiLine.Checked ? RegexOptions.Multiline : RegexOptions.None;
+            options |= cbIgnoreCase.Checked ? RegexOptions.IgnoreCase : RegexOptions.None;
 
-            if(rbIgnoreCase.Checked)
-            {
-                retVal = new Regex(tbRegEx.Text, RegexOptions.IgnoreCase);
-            }
-            else
-            {
-                retVal = new Regex(tbRegEx.Text);
-            }
+            retVal = cbIgnoreCase.Checked ? new Regex(tbRegEx.Text, options) : new Regex(tbRegEx.Text);
 
             return retVal;
+        }
+
+        // ------------------------------------------------
+
+        private void ClearMatches()
+        {
+            tbInput.Text = tbInput.Text;
+            tbInput.SelectionStart = tbInput.Text.Length;
+            tbInput.SelectionLength = 0;
+            tbInput.SelectionBackColor = Color.White;
+        }
+
+        // ------------------------------------------------
+
+        private void OnAbout(object sender, EventArgs e)
+        {
+            var dlg = new AboutJoeWareDlg();
+            dlg.ShowDialog();
+        }
+
+        // ------------------------------------------------
+
+        private void ClearInput(object sender, EventArgs e)
+        {
+            tbInput.Text = string.Empty;
+        }
+
+        // ------------------------------------------------
+
+        private void OnOptionsChange(object sender, EventArgs e)
+        {
+            ShowMatches();
+        }
+
+        // ------------------------------------------------
+
+        private void OnMatch(object sender, EventArgs e)
+        {
+            ShowMatches();
         }
     }
 }

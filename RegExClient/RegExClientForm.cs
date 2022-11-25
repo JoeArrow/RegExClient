99d3714d33590d172265d82769ac264128424d5f
Joe Arrowood <jarrow@ctvea.net>
2022-11-25 14:04:05 -0500

    Removed CW Notice

--- RegExClient/RegExClientForm.cs
+++ RegExClient/RegExClientForm.cs
@@ -1,12 +1,4 @@
-﻿#region © 2021 JoeWare.
-//
-// All rights reserved. Reproduction or transmission in whole or in part, in
-// any form or by any means, electronic, mechanical, or otherwise, is prohibited
-// without the prior written consent of the copyright owner.
-//
-#endregion
-
-using System;
+﻿using System;
 using System.IO;
 using System.Drawing;
 using System.Windows.Forms;

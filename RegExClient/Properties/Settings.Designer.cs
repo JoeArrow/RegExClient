42b269a9ec7d8e8025a3fdc4a7442dbaeb3ae5f1
Joe Arrowood <jarrow@ctvea.net>
2023-02-25 09:11:54 -0500

    Adding mor settings to set at startup

--- RegExClient/Properties/Settings.Designer.cs
+++ RegExClient/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RegExClient.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,42 @@ namespace RegExClient.Properties {
             }
             set {
                 this["IgnoreCase"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.15")]
+        public float RegexZoomFactor {
+            get {
+                return ((float)(this["RegexZoomFactor"]));
+            }
+            set {
+                this["RegexZoomFactor"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("960, 30")]
+        public global::System.Drawing.Size RegexSize {
+            get {
+                return ((global::System.Drawing.Size)(this["RegexSize"]));
+            }
+            set {
+                this["RegexSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("51")]
+        public int SplitterDist {
+            get {
+                return ((int)(this["SplitterDist"]));
+            }
+            set {
+                this["SplitterDist"] = value;
             }
         }
     }
